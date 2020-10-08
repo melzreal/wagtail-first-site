eecf53049ad91f584becc726c55d56adb2608a5b
melzreal <melissa.real@hotmail.com>
2020-10-08 16:55:19 +0100

    first commit

---
+++ firstProject/settings/dev.py
@@ -0,0 +1,31 @@
+from .base import *
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/btre')
+load_dotenv(os.path.join(project_folder, '.env'))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'wd@%w4e_q^1jh!(+u=k9e^e6jrst$n0!bx8w6tq0hhq748&*6n'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar'
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+INTERNAL_IPS = ("127.0.0.1, 172.17.0.1")
+
+try:
+    from .local import *
+except ImportError:
+    pass
